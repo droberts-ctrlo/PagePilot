125601f313b41efcb08d841dbaea1e40cf2968a6
Dave Roberts <d.roberts@ctrlo.com>
2025-03-11 13:30:50 +0000

    Initial setup

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { ignores: ["node_modules", "public", "webpack.config.js", "eslint.config.mjs", "babel.config.js", "jest.config.js"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  stylistic.configs.recommended,
+  { plugins: { "@stylistic": stylistic } },
+  {
+    rules: {
+      "@stylistic/indent": ["error", 4],
+      "@stylistic/semi": ["error", "always"],
+      "@stylistic/comma-dangle": ["error", "never"],
+    }
+  }
+];

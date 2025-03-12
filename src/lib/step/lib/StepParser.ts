b77d41a5edb8e60ac90522bed6c7d76dd321d5f6
Dave Roberts <d.roberts@ctrlo.com>
2025-03-12 14:37:13 +0000

    Refactored prefix for better compatibility

--- src/lib/step/lib/StepParser.ts
+++ src/lib/step/lib/StepParser.ts
@@ -10,16 +10,16 @@ import StepOptions from './StepOptions';
 export default <T extends HTMLElement = HTMLElement>(element: T, stepOptions: StepOptions = { prefix }) => {
     const data = element.dataset;
     const myPrefix = stepOptions.prefix;
-    if (!data[myPrefix + 'number']) throw new Error('No number found on element');
-    if (!data[myPrefix + 'content']) throw new Error('No content found on element');
+    if (!data[`${myPrefix}Number`]) throw new Error('No number found on element');
+    if (!data[`${myPrefix}Content`]) throw new Error('No content found on element');
     const step: Step<T> = {
         active: false,
         element,
         completed: false,
-        content: data[myPrefix + 'content'],
-        disabled: data[myPrefix + 'disabled'] === 'true',
-        number: parseInt(data[myPrefix + 'number']!),
-        title: data[myPrefix + 'title']
+        content: data[`${myPrefix}Content`],
+        disabled: data[`${myPrefix}Disabled`] === 'true',
+        number: parseInt(data[`${myPrefix}Number`]!),
+        title: data[`${myPrefix}Title`]
     };
     return step;
 };

2742a82890e389179325625386f27bea942245f5
Dave Roberts <d.roberts@ctrlo.com>
2025-03-12 16:40:56 +0000

    Updated failing test

--- src/lib/tooltip/lib/ToolTipConfig.ts
+++ src/lib/tooltip/lib/ToolTipConfig.ts
@@ -1,4 +1,4 @@
-import { CommonOptions } from "lib/common/options";
+import { CommonOptions } from "../../common/options";
 import TooltipButton from "./TooltipButton";
 
 export interface TooltipConfig<T extends HTMLElement = HTMLElement> extends CommonOptions {
--- src/lib/tooltip/lib/Tooltip.test.ts
+++ src/lib/tooltip/lib/Tooltip.test.ts
@@ -1,22 +1,38 @@
 import {describe, it, expect} from '@jest/globals';
 import Tooltip from './Tooltip';
+import { TooltipConfig } from './ToolTipConfig';
 
 // TODO: not sure what else to test here that isn't already part of Bootstrap
 describe('Tooltip', () => {
     it('Should error if the element passed in is undefined', () => {
-        //@ts-expect-error - Testing for error
-        expect(() => new Tooltip(undefined)).toThrowError('Element not found');
+        const config:TooltipConfig = {
+            //@ts-expect-error - Testing for error
+            element: undefined,
+            buttons: [],
+            prefix: 'pp'
+        };
+        expect(() => new Tooltip(config)).toThrowError('Element not found');
     });
 
     it('Should error if the element passed in does not have a content attribute', () => {
         const element = document.createElement('div');
-        expect(() => new Tooltip(element)).toThrowError('No content found on element');
+        const config:TooltipConfig = {
+            element,
+            buttons: [],
+            prefix: 'pp'
+        }
+        expect(() => new Tooltip(config)).toThrowError('No content found on element');
     });
 
     it('Should create a new tooltip', () => {
         const element = document.createElement('div');
         element.dataset['ppContent'] = 'Content';
-        const tooltip = new Tooltip(element);
+        const config:TooltipConfig = {
+            element,
+            buttons: [],
+            prefix: 'pp'
+        }
+        const tooltip = new Tooltip(config);
         expect(tooltip).toBeInstanceOf(Tooltip);
     });
 });
